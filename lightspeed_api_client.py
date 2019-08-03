e9fa9201d61fd33a04a859d8d3708357f8057aec
Daniël Mizrahi <dmizrahi@live.nl>
2019-08-03 19:57:10 +0200

    Added product_get method

--- lightspeed_api_client.py
+++ lightspeed_api_client.py
@@ -1,3 +1,5 @@
+import requests
+
 class LightspeedAPIClient:
     API_URL = 'https://api.webshopapp.com/nl/'
     API_key = ''
@@ -6,3 +8,24 @@ class LightspeedAPIClient:
     def __init__(self, API_key, API_secret):
         self.API_key = API_key
         self.API_secret = API_secret
+
+
+    def products_get(self, brand = '', limit = 50, page = 1, since_id = 0, created_at_min = '', created_at_max = '', updated_at_min = '', updated_at_max = ''):
+        API_url = self.API_URL + 'products.json'
+        if brand != '':
+            API_url = API_url + '?brand=' + str(brand)
+        API_url = API_url + '?limit=' + str(limit)
+        API_url = API_url + '?page=' + str(page)
+        API_url = API_url + '?since_id' + str(since_id)
+        if brand != '':
+            API_url = API_url + '?brand=' + str(brand)
+        if created_at_min != '':
+            API_url = API_url + '?created_at_min=' + str(created_at_min)
+        if created_at_max != '':
+            API_url = API_url + '?created_at_max=' + str(created_at_max)
+        if updated_at_min != '':
+            API_url = API_url + '?updated_at_min=' + str(updated_at_min)
+        if updated_at_max != '':
+            API_url = API_url + '?updated_at_max=' + str(updated_at_max)
+        return requests.get(API_url, auth=(self.API_key, self.API_secret)).json()
+        
