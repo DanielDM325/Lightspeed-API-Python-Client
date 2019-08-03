e1acd15e95f12f64e2922f67c6b2dbf8ab9598e4
Daniël Mizrahi <dmizrahi@live.nl>
2019-08-03 20:31:37 +0200

    Added variants_get method
    Returns variants

--- lightspeed_api_client.py
+++ lightspeed_api_client.py
@@ -10,7 +10,7 @@ class LightspeedAPIClient:
         self.API_secret = API_secret
 
 
-    def products_get(self, brand = '', limit = 50, page = 1, since_id = 0, created_at_min = '', created_at_max = '', updated_at_min = '', updated_at_max = ''):
+    def products_get(self, brand='', limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
         API_url = self.API_URL + 'products.json'
         if brand != '':
             API_url = API_url + '?brand=' + str(brand)
@@ -37,4 +37,31 @@ class LightspeedAPIClient:
 
     def products_get_id(self, id):
         API_url = self.API_URL + 'products/' + str(id) + '.json'
+        return requests.get(API_url, auth=(self.API_key, self.API_secret)).json()
+
+
+    def variants_get(self, product='', article_code='', ean='', sku='', hs='', limit=50, page=1, since_id='', created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
+        API_url = self.API_URL + 'variants.json'
+        if product != '':
+            API_url = API_url + '?product=' + str(product)
+        if article_code != '':
+            API_url = API_url + '?article_code=' + str(article_code)
+        if ean != '':
+            API_url = API_url + '?ean=' + str(ean)
+        if sku != '':
+            API_url = API_url + '?sku=' + str(sku)
+        if hs != '':
+            API_url = API_url + '?product=' + str(hs)
+        API_url = API_url + '?limit=' + str(limit)
+        API_url = API_url + '?page=' + str(page)
+        if since_id != '':
+            API_url = API_url + '?since_id=' + str(since_id)
+        if created_at_min!= '':
+            API_url = API_url + '?created_at_min=' + str(created_at_min)
+        if created_at_max != '':
+            API_url = API_url + '?created_at_max=' + str(created_at_max)
+        if updated_at_min != '':
+            API_url = API_url + '?updated_at_min=' + str(updated_at_min)
+        if updated_at_max != '':
+            API_url = API_url + '?updated_at_max=' + str(updated_at_max)
         return requests.get(API_url, auth=(self.API_key, self.API_secret)).json()
