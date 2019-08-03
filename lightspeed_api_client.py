60c1940e3c1fec4e14d2e47edd14288368fbc184
Daniël Mizrahi <dmizrahi@live.nl>
2019-08-03 20:36:21 +0200

    Added variants_count method
    Returns the number of variants

--- lightspeed_api_client.py
+++ lightspeed_api_client.py
@@ -65,3 +65,8 @@ class LightspeedAPIClient:
         if updated_at_max != '':
             API_url = API_url + '?updated_at_max=' + str(updated_at_max)
         return requests.get(API_url, auth=(self.API_key, self.API_secret)).json()
+
+
+    def variants_count(self):
+        API_url = self.API_URL + 'variants/count.json'
+        return requests.get(API_url, auth=(self.API_key, self.API_secret)).json()
