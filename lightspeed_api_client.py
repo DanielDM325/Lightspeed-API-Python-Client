fd7aab26f5f0e5a695d9d9fb6b52aa82460e4bce
Daniël Mizrahi <dmizrahi@live.nl>
2019-08-04 12:55:39 +0200

    Improved styling
    Unnecessary blank lines removed.

--- lightspeed_api_client.py
+++ lightspeed_api_client.py
@@ -1,12 +1,12 @@
 import requests
 
+
 class LightspeedAPIClient:
     API_URL = 'https://api.webshopapp.com/nl/'
     credentials = None
 
     def __init__(self, API_key, API_secret):
         self.credentials = (API_key, API_secret)
-
 
     def products_get(self, brand='', limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
         API_url = self.API_URL + 'products.json?'
@@ -26,17 +26,14 @@ class LightspeedAPIClient:
         if updated_at_max != '':
             API_url = API_url + 'updated_at_max=' + str(updated_at_max) + '&'
         return requests.get(API_url, auth=self.credentials).json()
-    
 
     def products_count(self):
         API_url = self.API_URL + 'products/count.json'
         return requests.get(API_url, auth=self.credentials).json()
 
-
     def products_get_id(self, id):
         API_url = self.API_URL + 'products/' + str(id) + '.json'
         return requests.get(API_url, auth=self.credentials).json()
-
 
     def variants_get(self, product='', article_code='', ean='', sku='', hs='', limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
         API_url = self.API_URL + 'variants.json?'
@@ -53,7 +50,7 @@ class LightspeedAPIClient:
         API_url = API_url + 'limit=' + str(limit) + '&'
         API_url = API_url + 'page=' + str(page) + '&'
         API_url = API_url + 'since_id=' + str(since_id) + '&'
-        if created_at_min!= '':
+        if created_at_min != '':
             API_url = API_url + 'created_at_min=' + str(created_at_min) + '&'
         if created_at_max != '':
             API_url = API_url + 'created_at_max=' + str(created_at_max) + '&'
@@ -63,16 +60,13 @@ class LightspeedAPIClient:
             API_url = API_url + 'updated_at_max=' + str(updated_at_max) + '&'
         return requests.get(API_url, auth=self.credentials).json()
 
-
     def variants_count(self):
         API_url = self.API_URL + 'variants/count.json'
         return requests.get(API_url, auth=self.credentials).json()
 
-
     def variants_get_id(self, id):
         API_url = self.API_URL + 'variants/' + str(id) + '.json'
         return requests.get(API_url, auth=self.credentials).json()
-
 
     def metafields_get(self, limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
         API_url = self.API_URL + 'metafields.json?'
@@ -89,11 +83,9 @@ class LightspeedAPIClient:
             API_url = API_url + 'updated_at_max=' + str(updated_at_max) + '&'
         return requests.get(API_url, auth=self.credentials).json()
 
-
     def metafields_count(self):
         API_url = self.API_URL + 'metafields/count.json'
         return requests.get(API_url, auth=self.credentials).json()
-
 
     def metafields_get_id(self, id):
         API_url = self.API_URL + 'metafields/' + str(id) + '.json'
