6c692fa299b412badf37e3c7853d1e614ad6706b
Daniël Mizrahi <dmizrahi@live.nl>
2019-08-05 13:49:55 +0200

    Added metafields_update method
    Updates a metafield for a given metafield id.

--- lightspeed_api_client.py
+++ lightspeed_api_client.py
@@ -95,8 +95,12 @@ class LightspeedAPIClient:
     def metafields_create(self, owner_type, owner_id, key, value):
         API_url = self.API_URL + 'metafields.json'
         payload = {'metafield[ownerType]' : owner_type, 'metafield[ownerId]' : str(owner_id), 'metafield[key]' : str(key), 'metafield[value]' : str(value), 'metafield[ownerResource]' : str(owner_id)}
-        print(payload)
         return requests.post(API_url, data=payload, auth=self.credentials).json()
+
+    def metafields_update(self, metafield_id, owner_type, owner_id, key, value):
+        API_url = self.API_URL + 'metafields/' + str(metafield_id) + '.json'
+        payload = {'metafield[ownerType]' : owner_type, 'metafield[ownerId]' : str(owner_id), 'metafield[key]' : str(key), 'metafield[value]' : str(value), 'metafield[ownerResource]' : str(owner_id)}
+        return requests.put(API_url, data=payload, auth=self.credentials).json()
     
     def product_images_get(self, id):
         API_url = self.API_URL + 'products/' + str(id) + '/images.json'
