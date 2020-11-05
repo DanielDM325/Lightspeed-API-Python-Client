04083ed7fc9b6ac6d5eceeb74b56f4eb00166ca0
Daniël Mizrahi <dmizrahi@live.nl>
2020-11-05 15:22:04 +0100

    Added products_attribute_get method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -64,6 +64,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def products_attribute_get(self, product_id):
+        API_url = self.API_URL + 'products/' + str(product_id) + '/attributes.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def products_metafield_get(self, metafield_id, limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
         API_url = self.API_URL + 'products/' + str(metafield_id) + '/metafields.json?'
         API_url = API_url + 'limit=' + str(limit) + '&'
