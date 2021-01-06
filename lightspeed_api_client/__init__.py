4638c8b6d5874d7a191eec37b767392ae17249e4
Daniël Mizrahi <dmizrahi@live.nl>
2021-01-06 20:58:46 +0100

    Added checkout_validate method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -841,6 +841,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def checkout_validate(self, checkout_id):
+        API_url = self.API_URL + 'checkouts/' + str(checkout_id) + '/validate.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def customers_get(self, page=1, limit=50):
         API_url = self.API_URL + 'customers.json'
         filters = {
