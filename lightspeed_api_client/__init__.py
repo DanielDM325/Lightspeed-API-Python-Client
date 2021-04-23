6f94530f64ef900806c8ae930a844549894b096a
Daniël Mizrahi <dmizrahi@live.nl>
2021-04-23 17:44:04 +0200

    products_get now uses a param arg as a filter

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -13,24 +13,19 @@ class LightspeedAPIClient:
     def __init__(self, API_key, API_secret):
         self.credentials = (API_key, API_secret)
 
-    def products_get(self, brand='', limit=50, page=1, since_id=0, created_at_min='', created_at_max='', updated_at_min='', updated_at_max=''):
-        API_url = self.API_URL + 'products.json?'
-        if brand != '':
-            API_url = API_url + 'brand=' + str(brand) + '&'
-        API_url = API_url + 'limit=' + str(limit) + '&'
-        API_url = API_url + 'page=' + str(page) + '&'
-        API_url = API_url + 'since_id' + str(since_id) + '&'
-        if brand != '':
-            API_url = API_url + 'brand=' + str(brand) + '&'
-        if created_at_min != '':
-            API_url = API_url + 'created_at_min=' + str(created_at_min) + '&'
-        if created_at_max != '':
-            API_url = API_url + 'created_at_max=' + str(created_at_max) + '&'
-        if updated_at_min != '':
-            API_url = API_url + 'updated_at_min=' + str(updated_at_min) + '&'
-        if updated_at_max != '':
-            API_url = API_url + 'updated_at_max=' + str(updated_at_max) + '&'
-        response = requests.get(API_url, auth=self.credentials)
+    def products_get(self, brand=None, limit=50, page=1, since_id=0, created_at_min=None, created_at_max=None, updated_at_min=None, updated_at_max=None):
+        API_url = self.API_URL + 'products.json'
+        filters = {
+            'brand': brand,
+            'limit': limit,
+            'page': page,
+            'since_id': since_id,
+            'created_at_min': created_at_min,
+            'created_at_max': created_at_max,
+            'updated_at_min': updated_at_min,
+            'updated_at_max': updated_at_max
+        }
+        response = requests.get(API_url, params=filters, auth=self.credentials)
         self.update_status(response)
         if response.status_code == 200:
             return response.json()
