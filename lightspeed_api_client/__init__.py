d1f097f0f06c97b383675bca7fb014957b5eea12
Daniël Mizrahi <dmizrahi@live.nl>
2021-04-25 22:30:44 +0200

    Added filter_values_count method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1285,6 +1285,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def filter_values_count(self, filter_id):
+        API_url = self.API_URL + 'filters/' + str(filter_id) + '/values/count.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
