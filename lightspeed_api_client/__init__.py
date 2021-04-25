2de418a9f9e88e1ba102d326eac773fe4bf24ac8
Daniël Mizrahi <dmizrahi@live.nl>
2021-04-25 22:31:50 +0200

    Added filter_values_get_id method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1294,6 +1294,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def filter_values_get_id(self, filter_id, filter_value_id):
+        API_url = self.API_URL + 'filters/' + str(filter_id) + '/values/' + str(filter_value_id) + '.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
