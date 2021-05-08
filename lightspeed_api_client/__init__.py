c283728efc04848e1aa1b8d49f8fc6055987c0d3
Daniël Mizrahi <dmizrahi@live.nl>
2021-05-08 02:15:00 +0200

    Added shipment_method_count method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1364,6 +1364,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def shipment_method_count(self):
+        API_url = self.API_URL + 'shippingmethods/count.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
