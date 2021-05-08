fceba5901706d5fc5348640d1336b177c78fb24a
Daniël Mizrahi <dmizrahi@live.nl>
2021-05-08 02:15:15 +0200

    Added shipment_method_get_id method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1373,6 +1373,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def shipment_method_get_id(self, shipment_method_id):
+        API_url = self.API_URL + 'shippingmethods/' + str(shipment_method_id) + '.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
