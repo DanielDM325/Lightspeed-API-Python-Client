bd6766f05e0eb8694f54a9dd915006d3394bf812
Daniël Mizrahi <dmizrahi@live.nl>
2021-05-16 00:42:14 +0200

    Added payment_method_count method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1393,6 +1393,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def payment_method_count(self):
+        API_url = self.API_URL + 'paymentmethods/count.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
