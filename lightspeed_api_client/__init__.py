8a8ca8cbda836352eb1de295d220dce7a87e0063
Daniël Mizrahi <dmizrahi@live.nl>
2021-07-14 21:44:40 +0200

    Added blog_articles_count method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1539,6 +1539,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def blog_articles_count(self):
+        API_url = self.API_URL + 'blogs/articles/count.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
