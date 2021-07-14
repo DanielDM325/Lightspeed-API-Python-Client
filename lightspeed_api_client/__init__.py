a55626e20b49aec5ebc8b8d7662638121fea3b0f
Daniël Mizrahi <dmizrahi@live.nl>
2021-07-14 22:58:25 +0200

    Added textpages_get_id method

--- lightspeed_api_client/__init__.py
+++ lightspeed_api_client/__init__.py
@@ -1590,6 +1590,15 @@ class LightspeedAPIClient:
         else:
             return None
 
+    def textpages_get_id(self, textpage_id):
+        API_url = self.API_URL + 'textpages/' + str(textpage_id) + '.json'
+        response = requests.get(API_url, auth=self.credentials)
+        self.update_status(response)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
     def update_status(self, response):
         self.rate_limit_remaining = response.headers['X-RateLimit-Remaining'].split('/')
         self.rate_limit_reset = response.headers['X-RateLimit-Reset'].split('/')
